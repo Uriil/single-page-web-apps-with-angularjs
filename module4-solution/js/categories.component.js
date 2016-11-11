6e4e345fc4e4bd0ab66244a4943bf96d149acf5b
Nikita Gusev <nikita.gusev.1987@gmail.com>
2016-11-11 18:53:39 +0100

    Added module 5 start point

--- module4-solution/js/categories.component.js
+++ module4-solution/js/categories.component.js
@@ -3,10 +3,8 @@
     function CategoriesComponent(){
         return {
             templateUrl: 'shoppingList.html',
-            controller: ShoppingListComponentController,
             bindings: {
                 items: '<',
-                myTitle: '@title',
                 onRemove: '&'
             }
         }
